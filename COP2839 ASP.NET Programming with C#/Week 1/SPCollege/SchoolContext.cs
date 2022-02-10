c069a5dcfac6518ec0df1aa2085d6c211d98a48e
Tristan Izlar <59484088+tizlar@users.noreply.github.com>
2022-02-10 18:16:46 -0500

    Uploading COP2839 Course Work

---
+++ COP2839 ASP.NET Programming with C#/Week 1/SPCollege/SchoolContext.cs
@@ -0,0 +1,21 @@
+﻿using SPCollege.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace SPCollege.DAL
+        //For EntityFramework
+        public SchoolContext() : base("SchoolContext")
+{
+}
+//For EntityFramework Core
+//public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+//{
+//}
+public DbSet<Student> Students { get; set; }
+public DbSet<Enrollment> Enrollments { get; set; }
+public DbSet<Course> Courses { get; set; }
+//For Entity Framework
+protected override void OnModelCreating(DbModelBuilder modelBuilder)
+{
+    modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+}
